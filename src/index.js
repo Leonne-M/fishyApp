0100e4fb15d2248a97f5f1c6c1727f62996286dd
leonne <minterbottsyt@gmail.com>
2024-04-11 13:04:38 +0300

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 function getdata(){
-    let x=0
-    console.log(x)
+    const myHeaders = new Headers();
+    myHeaders.append("X-RapidAPI-Key", "aa1565a7ffmsh4ea9bf84985da6cp1ece19jsnc8a6912cd422");
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = "\n";
+    
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+     
+    };
+    
+    fetch("https://fish-species.p.rapidapi.com/fish_api/fishes", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
 }
